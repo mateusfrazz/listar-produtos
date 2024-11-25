73a54e9175fee6ff2c29b1dcc0670f29845a5c0c
mateusfrazz <mateusfrazao12345@gmail.com>
2024-11-25 14:52:55 -0300

    Upando o projeto de forma remota

---
+++ build/produtos.js
@@ -0,0 +1,20 @@
+"use strict";
+//declarando um novo objeto com os parametros definidos em Produto
+const produtos = [
+    { nome: "Arroz", preco: 10, emEstoque: false },
+    { nome: "Feijao", preco: 20, emEstoque: true },
+    { nome: "Carne", preco: 30, emEstoque: true },
+    { nome: "batata", preco: 40, emEstoque: false },
+];
+//Função para listar produtos que estiver em estoque
+function listarProdutosEstoque() {
+    const produtoEstoque = produtos.filter((produto) => produto.emEstoque === true);
+    return produtoEstoque;
+}
+console.log(listarProdutosEstoque());
+//função para listar produtos com o preço abaixo de 30
+function listarProdutosPrecoBaixo() {
+    const produtoBaixoValor = produtos.filter((produto) => produto.preco < 30);
+    return produtoBaixoValor;
+}
+console.log(listarProdutosPrecoBaixo());
